91f3e30a4e8104f982ba5216539e8cbc0b2ed74c
Tristan McCormick <tristanjmccormick@gmail.com>
2018-10-25 18:35:40 -0700

    Initial commit on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import map from 'lodash/map';
+import filter from 'lodash/filter';
 
 import { DataConsumer } from '../DataProvider';
 import { colors } from './PatternLibrary/constants';
-
-const DeltaStyled = styled.div`
-  width: 220px;
-  min-height: 160px;
-  margin: 10px;
-  box-shadow: 0 7px 11px rgba(0, 0, 0, 0.17), 0 3px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 20px;
-  display: flex;
-  align-content: center;
-  flex-wrap: wrap;
-  padding: 15px;
-`;
+import { CardContainer } from './PatternLibrary';
 
 const CardHeader = styled.div`
   height: 40px;
@@ -30,19 +20,54 @@ const CardHeader = styled.div`
 `;
 
 
-const Home = ({students}) => {
-  if (!students) {
-    return null;
+class Home extends React.Component {
+  getStudentActionList = (student) => {
+    const { actions } = this.props;
+    const actionsForStudent = filter(actions, (a) => {
+      return a.student.id === student.id;
+    })
+
+    if (actionsForStudent.length === 0) {
+      return (
+        <div>
+          Create your first action!
+        </div>
+      )
+    }
+    return (
+      <div>
+        {map(actionsForStudent, (a) => {
+          return (
+            <CardContainer>
+              {a.note}
+            </CardContainer>
+          )
+        })}
+      </div>
+    )
   }
-  return (
-    <div>
-      {map(students.slice(0,10), (s) => {
-        return (
-          <h4>{s.last_name} {s.first_name}</h4>
-        )
-      })}
-    </div>
-  )
+
+
+  render() {
+    const { students, actions } = this.props;
+
+    if (!students || !actions) {
+      return null;
+    }
+
+    return (
+      <div>
+        {map(students.slice(0,10), (s) => {
+          return (
+            <div>
+              <CardHeader>{s.last_name} {s.first_name}</CardHeader>
+              {this.getStudentActionList(s)}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 
--- src/components/PatternLibrary/index.js
+++ src/components/PatternLibrary/index.js
@@ -1,4 +1,5 @@
 import Button from './Button.js';
+import CardContainer from './CardContainer.js';
 import Error from './Error.js';
 import GoogleLogo from './GoogleLogo.js';
 import Loading from './Loading.js';
@@ -6,6 +7,7 @@ import Logo from './Logo.js';
 
 export {
   Button,
+  CardContainer,
   Error,
   GoogleLogo,
   Loading,
