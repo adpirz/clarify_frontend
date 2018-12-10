5c99d5d0619c0c2864ce5540200749544b165dc4
adpirz <ad.pirz@gmail.com>
2018-12-09 17:24:33 -0800

    styling changes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { DataConsumer } from "../DataProvider";
-import { Error } from "./PatternLibrary";
+import { Error, Button } from "./PatternLibrary";
 import GoogleAuth from "./GoogleAuth";
 import styled from "styled-components";
-import { lighten } from "polished";
+import { lighten, darken } from "polished";
 
 import { colors } from "./PatternLibrary/constants";
 
@@ -11,7 +11,7 @@ const CLEVER_CLIENT_ID = process.env.REACT_APP_CLEVER_CLIENT_ID;
 const CLEVER_REDIRECT_URL = process.env.REACT_APP_CLEVER_REDIRECT_URL || "http://localhost:3000";
 
 const LoginFormContainer = styled.div`
-  width: 400px;
+  width: 450px;
   min-height: 300px;
   border-radius: 20px;
   background: linear-gradient(180deg, ${lighten(0.6, "grey")} 70%, ${lighten(0.47, "grey")});
@@ -25,9 +25,9 @@ const LoginFormContainer = styled.div`
 `;
 
 const LoginHelperText = styled.div`
-  color: ${lighten(0.7, colors.black)};
+  color: ${lighten(0.6, colors.black)};
   font-size: 0.75em;
-  margin: auto 0 0;
+  margin: 20px;
   text-align: center;
   line-height: 1.42em;
 `;
@@ -35,8 +35,8 @@ const LoginHelperText = styled.div`
 const LoginHeader = styled.h1`
   font-weight: 400;
   color: ${lighten(0.45, colors.black)};
-  font-size: 1.8em;
-  margin: 20px auto;
+  font-size: 1em;
+  margin: 30px auto 5px;
 `;
 
 const EmailLink = styled.a`
@@ -59,6 +59,18 @@ const CleverImage = styled.img`
   &:hover {
     box-shadow: 0 0 3px 3px rgba(66, 133, 244, 0.3);
   }
+`;
+
+const LoginInput = styled.input`
+  width: 60%;
+  height: 30px;
+  font-size: 0.9em;
+  padding: 2px 0.7em;
+  margin: 5px;
+  border: 1px solid ${darken(0.15, "white")};
+  border-radius: 7px;
+  background-color: ${darken(0.02, "white")};
+  box-shadow: inset 1px 1px 1px 0px rgba(0, 0, 0, 0.08);
 `;
 
 class Login extends React.Component {
@@ -104,7 +116,7 @@ class Login extends React.Component {
       "&district_id=5b2ad81a709e300001e2cd7a";
 
     return (
-      <div style={{ margin: "25vh auto" }}>
+      <div style={{ margin: "20vh auto" }}>
         <LoginFormContainer>
           <LoginHeader>Login with Google</LoginHeader>
           <GoogleAuth onSuccess={this.googleLogin} onFailure={err => console.log(err)} />
@@ -113,21 +125,21 @@ class Login extends React.Component {
             <CleverImage src="./clever_login_button.png" />
           </a>
           <LoginHeader>Login with Clarify</LoginHeader>
-          <label htmlFor="username-input">Username</label>
-          <input
+          <LoginInput
             type="text"
             id="username-input"
+            placeholder="Username"
             value={this.state.username}
             onChange={this.handleUsernameUpdate}
           />
-          <label htmlFor="password-input">Password</label>
-          <input
+          <LoginInput
             type="password"
             id="password-input"
+            placeholder="Password"
             value={this.state.password}
             onChange={this.handlePasswordUpdate}
           />
-          <button onClick={this.initiateClarifyLogin}>Log in</button>
+          <Button onClick={this.initiateClarifyLogin}>Log in</Button>
           <Error>{errorNode}</Error>
           <LoginHelperText>
             This should be the same account you use to login with <strong>Illuminate</strong>.
