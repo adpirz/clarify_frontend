87b073d7735c6d606f7eb3fdea4025119bd385e4
adpirz <ad.pirz@gmail.com>
2019-01-09 13:01:02 -0800

    added StudentDetail route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,17 @@ import reduce from "lodash/reduce";
 import without from "lodash/without";
 import { NavLink } from "react-router-dom";
 
-import { Segment, Header, Grid, Menu, Icon, Card, Transition, Form } from "semantic-ui-react";
+import {
+  Segment,
+  Header,
+  Grid,
+  Menu,
+  Icon,
+  Card,
+  Transition,
+  Form,
+  Container,
+} from "semantic-ui-react";
 
 import { ActionCard, DeltaCard } from "../components/PatternLibrary";
 
@@ -125,88 +135,92 @@ class Home extends React.Component {
     }
 
     return (
-      <Grid columns={16}>
-        <Grid.Column width={14}>
-          <Menu color="red" inverted pointing size="massive">
-            <Menu.Item header>
-              <Header inverted as="h1">
-                Next Steps
-              </Header>
-            </Menu.Item>
-            <Menu.Menu position="right">
-              <Menu.Item>
-                <Icon name="sticky note" />
-                Note
+      <Grid container padded columns={1}>
+        <Grid.Row>
+          <Grid.Column>
+            <Menu color="red" inverted pointing size="massive">
+              <Menu.Item header>
+                <Header inverted as="h1">
+                  Next Steps
+                </Header>
               </Menu.Item>
-              <Menu.Item>
-                <Icon name="phone" />
-                Call
-              </Menu.Item>
-              <Menu.Item>
-                <Icon name="chat" />
-                Message
-              </Menu.Item>
-            </Menu.Menu>
-          </Menu>
-        </Grid.Column>
-        <Grid.Column width={14}>
-          {map(studentViewModels.slice(0, 3), (studentViewModel, i) => {
-            const { student } = studentViewModel;
-            const activeStudent = this.state.selectedStudentId === student.id;
-            return (
-              <Segment basic>
-                <Menu size="massive" inverted pointing attached="top">
-                  <Menu.Item header as={NavLink} to={`/student/${student.id}`}>
-                    <Header inverted as="h2">
-                      {student.first_name} {student.last_name[0]}
-                    </Header>
-                  </Menu.Item>
-                  <Menu.Menu position="right">
-                    {map(
-                      [
-                        { name: "Note", value: "note", icon: "sticky note" },
-                        { name: "Call", value: "call", icon: "phone" },
-                        { name: "Message", value: "message", icon: "chat" },
-                      ],
-                      action => (
-                        <Menu.Item
-                          link
-                          onClick={this.handleTypeSelection.bind(this, student.id, action.value)}
-                          active={
-                            this.state.type === action.value &&
-                            this.state.selectedStudentId === student.id
-                          }
-                        >
-                          <Icon name={action.icon} />
-                          {action.name}
-                        </Menu.Item>
-                      )
-                    )}
-                  </Menu.Menu>
-                </Menu>
-                <Transition.Group animation="slide down">
-                  {activeStudent ? (
-                    <Segment inverted attached="bottom">
-                      <Form inverted>
-                        <Form.Field
-                          control="textarea"
-                          label="Note"
-                          placeholder="I noticed that...."
-                        />
-                        <Form.Field control="input" type="checkbox" label="Publicly visible?" />
-                      </Form>
-                    </Segment>
-                  ) : null}
-                </Transition.Group>
-                <Segment basic attached="bottom">
-                  <Card.Group itemsPerRow={3}>
-                    {this.getStudentDeltaList(studentViewModel)}
-                  </Card.Group>
+              <Menu.Menu position="right">
+                <Menu.Item>
+                  <Icon name="sticky note" />
+                  Note
+                </Menu.Item>
+                <Menu.Item>
+                  <Icon name="phone" />
+                  Call
+                </Menu.Item>
+                <Menu.Item>
+                  <Icon name="chat" />
+                  Message
+                </Menu.Item>
+              </Menu.Menu>
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            {map(studentViewModels.slice(0, 3), (studentViewModel, i) => {
+              const { student } = studentViewModel;
+              const activeStudent = this.state.selectedStudentId === student.id;
+              return (
+                <Segment basic>
+                  <Menu size="massive" inverted pointing attached="top">
+                    <Menu.Item header as={NavLink} to={`/student/${student.id}`}>
+                      <Header inverted as="h2">
+                        {student.first_name} {student.last_name[0]}
+                      </Header>
+                    </Menu.Item>
+                    <Menu.Menu position="right">
+                      {map(
+                        [
+                          { name: "Note", value: "note", icon: "sticky note" },
+                          { name: "Call", value: "call", icon: "phone" },
+                          { name: "Message", value: "message", icon: "chat" },
+                        ],
+                        action => (
+                          <Menu.Item
+                            link
+                            onClick={this.handleTypeSelection.bind(this, student.id, action.value)}
+                            active={
+                              this.state.type === action.value &&
+                              this.state.selectedStudentId === student.id
+                            }
+                          >
+                            <Icon name={action.icon} />
+                            {action.name}
+                          </Menu.Item>
+                        )
+                      )}
+                    </Menu.Menu>
+                  </Menu>
+                  <Transition.Group animation="slide down">
+                    {activeStudent ? (
+                      <Segment inverted attached="bottom">
+                        <Form inverted>
+                          <Form.Field
+                            control="textarea"
+                            label="Note"
+                            placeholder="I noticed that...."
+                          />
+                          <Form.Field control="input" type="checkbox" label="Publicly visible?" />
+                        </Form>
+                      </Segment>
+                    ) : null}
+                  </Transition.Group>
+                  <Segment basic attached="bottom">
+                    <Card.Group itemsPerRow={3}>
+                      {this.getStudentDeltaList(studentViewModel)}
+                    </Card.Group>
+                  </Segment>
                 </Segment>
-              </Segment>
-            );
-          })}
-        </Grid.Column>
+              );
+            })}
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
